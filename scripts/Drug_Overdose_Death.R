e0b45753f2c678c1818cc8e343913774b48891a8
Alex Chau <alex_chau1@yahoo.com>
2019-05-24 18:30:25 -0700

    Edit chart files

--- scripts/Drug_Overdose_Death.R
+++ scripts/Drug_Overdose_Death.R
@@ -11,15 +11,15 @@ plot_drug_overdose <- function(drug_overdose){
 # Number of drug overdose deaths over the course of 2015 - 2018 by state
 
 drug_overdose_states <- drug_overdose %>%
-  select(ï..State, Year, Indicator, Data.Value) %>%
-  rename(State = ï..State) %>%
+  select(State.Name, Year, Indicator, Data.Value) %>%
+  mutate(State.Name = state.abb[match(State.Name ,state.name)]) %>%
   filter(Indicator == c("Number of Drug Overdose Deaths")) %>%
-  group_by(State) %>%
+  group_by(State.Name) %>%
   mutate(count = sum(as.numeric(Data.Value)))
 
 # Hover information
 
-drug_overdose_states$hover <- with(drug_overdose_states, paste(State, "<br>", Indicator, ":", count, "<br>"))
+drug_overdose_states$hover <- with(drug_overdose_states, paste(State.Name, "<br>", Indicator, ":", count, "<br>"))
 
 # give state boundaries a white border
 
@@ -36,7 +36,7 @@ g <- list(
 
 plot_map <- plot_geo(drug_overdose_states, locationmode = "USA-states") %>%
   add_trace(
-    z = ~count, text = ~hover, locations = ~State,
+    z = ~count, text = ~hover, locations = ~State.Name,
     color = ~count, colors = "Blues"
   ) %>%
   colorbar(title = "Death count") %>%
--- scripts/drug_use_by_age_data_plot.R
+++ scripts/drug_use_by_age_data_plot.R
@@ -16,14 +16,16 @@ drug_age_data_whole <- drug_age_data %>%
   select(age, n, alcohol.use, marijuana.use, cocaine.use, crack.use, heroin.use, hallucinogen.use, inhalant.use,
          pain.releiver.use, oxycontin.use, tranquilizer.use, stimulant.use, meth.use, sedative.use
   ) %>%   
-  mutate_at(vars(ends_with(".use")), funs((./100) * n))
+  rename(Number_of_users = n) %>%
+  mutate_at(vars(ends_with(".use")), funs((./100) * Number_of_users))
 
 # Plot data
 
 plot_drug_age <- ggplot(drug_age_data_whole) +
   geom_col(
-    mapping = aes(x = age, y = n)
-  )
+    mapping = aes(x = age, y = Number_of_users)
+  ) +
+  ggtitle("Number of users by age group")
 
 plot(plot_drug_age)
 }
